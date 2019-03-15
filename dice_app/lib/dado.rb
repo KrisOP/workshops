ec5c71244d64d8a861e6ca3b352f5f9f28f6f089
KrisOP <kortega070@gmail.com>
2019-03-15 11:39:52 -0600

    tercera prueba TDD

--- dice_app/lib/dado.rb
+++ dice_app/lib/dado.rb
@@ -5,6 +5,11 @@ class Dado
 
     def resultado
         salida_dado = rand (1..6)
-        #salida_dado
+        salida_dado
+    end
+
+    def random_player
+        salida_player = rand (1..3)
+        salida_player
     end
 end
--- dice_app/spec/dado_spec.rb
+++ dice_app/spec/dado_spec.rb
@@ -12,6 +12,14 @@ describe  Dado do
         dado=Dado.new
         expect(subject.resultado).to be_between(1,6)
         end
+
     end
+
+    describe '#random' do
+        it 'random player' do
+        dado=Dado.new
+        expect(subject.random_player).to be_between(1,3)
+        end
+    end 
 end
 
