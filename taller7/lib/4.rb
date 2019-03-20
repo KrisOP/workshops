fb0619f06101426cf4c05e3ed0912fd9cb0f1bdb
KrisOP <kortega070@gmail.com>
2019-03-20 13:41:14 -0600

    refactorizando 4.rb

---
+++ taller7/lib/4.rb
@@ -0,0 +1,25 @@
+class Mail
+    def initialize(email, fancy = false)
+      @email = email
+      @fancy = fancy
+    end
+  
+    def send_message
+      if fancy
+        MailServer.connect("http://mixmax.com", api_key: "f20506xx808c").send_message({ to: @email, body: "Welcome to MyProduct, fancy person!" })
+      else
+        MailServer.connect("http://mixmax.com", api_key: "f20506xx808c").send_message({ to: @email, body: "Welcome to MyProduct" })
+      end
+    end
+  end
+
+  def send_message
+    mail_server = MailServer.connect("http://mixmax.com", api_key: "f20506xx808c")
+    base_body = "Welcome to MyProduct"
+
+    body = fancy ? "#{base_body }, fancy person!": base_body
+
+    mail_server.send_message({to: @email,body: base_body})
+
+  end
+  end
