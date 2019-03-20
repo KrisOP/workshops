a88110dff1088e4b5adde823cc28483899b57635
KrisOP <kortega070@gmail.com>
2019-03-20 11:13:29 -0600

    refactorizando primer ejercicio

--- taller7/lib/1.rb
+++ taller7/lib/1.rb
@@ -1,3 +1,10 @@
 def send_email_to(user)
     Mail.new(user.email.strip).send_message
   end
+
+
+def send_email_to (user)
+  email = user.email.strip
+  mail = mail.new(email)
+  mail.send_message
+end
