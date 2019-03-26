b7682242bfcec44ee697a75eadfefb44aab926ba
KrisOP <kortega070@gmail.com>
2019-03-26 11:10:03 -0600

    configurando servidor condicionando salida

--- taller9/server.rb
+++ taller9/server.rb
@@ -4,10 +4,16 @@ server = TCPServer.new(2345)
 
 socket = server.accept
 
-socket.puts "What do you say?"
+loop do 
+    socket.puts "What do you say?"
+    they_said = socket.gets.chomp
 
-they_said = socket.gets.chomp
+    if they_said == 'exit'
+        socket.puts "You said: #{they_said}. Goodbye!"
+        socket.close
+        
+    end
+    break if they_said == 'exit'
+end 
 
-socket.puts "You said: #{they_said}. Goodbye!"
 
-socket.close
